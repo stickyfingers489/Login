34634a1a6a582f56be34c74a95fedb789b4d1b32
stickyfingers489 <86178881+stickyfingers489@users.noreply.github.com>
2022-03-28 12:37:40 +0300

    Initial Commit

---
+++ login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  login
+//
+//  Created by stickerfinger489 on 28/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by stickerfinger489 on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
