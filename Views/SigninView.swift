2832969212e1628b1e59aa93dc4483e1c5729009
stickyfingers489 <mutuadennis56@gmai.com>
2022-04-05 09:26:45 +0300

    second Commit

--- Views/SigninView.swift
+++ Views/SigninView.swift
@@ -8,20 +8,19 @@
 import SwiftUI
 
 struct SigninView: View {
-    enum Field: Hashable{
+    enum Field: Hashable {
         case email
         case password
     }
     @State var email = ""
     @State var password = ""
     @FocusState var focusedField: Field?
-   // @State var circleY: CGFloat = 0
-  //  @State var emailY: CGFloat = 0
-   // @State var passwordY: CGFloat = 0
-   // @State var circleColor: Color = .blue
+    @State var circleY: CGFloat = 0
+    @State var emailY: CGFloat = 0
+    @State var passwordY: CGFloat = 0
+    @State var circleColor: Color = .blue
     @State var appear = [false, false, false]
-    //@EnvironmentObject var model: Model
-    @AppStorage("isLogged") var isLogged = false
+   
 
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
@@ -29,7 +28,7 @@ struct SigninView: View {
                 .font(.largeTitle).bold()
                 .opacity(appear[0] ? 1 : 0)
                 .offset(y: appear[0] ? 0 : 20)
-            Text("Access 120+ hours of courses, tutorials and livestreams")
+            Text("Get your Orders Completed by qualified Tutors")
                 .font(.headline)
                 .opacity(appear[1] ? 1 : 0)
                 .offset(y: appear[1] ? 0 : 20)
@@ -43,10 +42,11 @@ struct SigninView: View {
                     .disableAutocorrection(true)
                     .focused($focusedField, equals: .email)
                     .shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
-                    .overlay(geometry)
-                  //  .onPreferenceChange(CirclePreferenceKey.self) { value in
-                     //   emailY = value
-                    //  circleY = value
+                   .overlay(geometry)
+                   .onPreferenceChange(CirclePreferenceKey.self) {
+                value in
+                      emailY = value
+                     circleY = value
                     }
                 SecureField("Password", text: $password)
                     .inputStyle(icon: "lock")
@@ -54,11 +54,12 @@ struct SigninView: View {
                     .focused($focusedField, equals: .password)
                     .shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
                     .overlay(geometry)
-                  //  .onPreferenceChange(CirclePreferenceKey.self) { value in
-                      //  passwordY =  value
+                  .onPreferenceChange(CirclePreferenceKey.self) { value in
+                       passwordY =  value
                     }
+                            
                 Button {
-                    isLogged = true
+                    
                 } label: {
                     Text("Sign in")
                         .frame(maxWidth: .infinity)
@@ -74,13 +75,66 @@ struct SigninView: View {
                 
                 HStack {
                     Text("No account yet?")
+                    Button {
+                        
+                    } label: {
+                        Text("**Sign up**")
+                    }
+                }
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .accentColor(.secondary)
+            }
+            .opacity(appear[2] ? 1 : 0)
+            .offset(y: appear[2] ? 0 : 20)
+        }
+        .padding(20)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .background(
+            Circle().fill(circleColor)
+                .frame(width: 68, height: 68)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .offset(y: circleY)
+            )
+                .coordinateSpace(name: "container")
+                .strokeStyle(cornerRadius: 30)
+                .onChange(of: focusedField) { value in
+                    withAnimation {
+                        if value == .email {
+                            circleY = emailY
+                            circleColor = .blue
+                        } else {
+                            circleY = passwordY
+                            circleColor = .red
+                        }
+                    }
+                }
+                .onAppear {
+                    withAnimation(.spring().delay(0.1)) {
+                        appear[0] = true
+                    }
+                    withAnimation(.spring().delay(0.2)) {
+                        appear[1] = true
+                    }
+                    withAnimation(.spring().delay(0.3)) {
+                        appear[2] = true
+                    }
+                }
+            }
+            
+            var geometry: some View {
+                GeometryReader { proxy in
+                    Color.clear.preference(key: CirclePreferenceKey.self, value: proxy.frame(in: .named("container")).minY)
+                }
+            }
+        }
                    
 
 struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
-            SignInView()
-               // .environmentObject(Model())
+            SigninView()
+                //.environmentObject(Model())
         }
     }
 }
